32c2262ac825c8a8709869739d8ae3a09c810a3e
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-13 06:23:30 +0600

    the set up of context is almost done

--- Ecommerce Project/src/Context/EcomContext.jsx
+++ Ecommerce Project/src/Context/EcomContext.jsx
@@ -1,3 +1,13 @@
-import { createContext } from "react";
+import { Children, createContext, useState } from "react";
 
+export const EcomContext = createContext(null);
+
+export const EcomConProvider = ({props}) => {
+    const [addcart, setAddcart] = useState(null)
+    return (
+        <EcomContext.Provider value={{addcart, setAddcart}}>
+            {props.Children}
+        </EcomContext.Provider>
+    )
+}
 
